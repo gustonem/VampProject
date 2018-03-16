7f483d1bfdd1a67b46cb18c0c9f9a6ea979c375e
gustonem <gustonem@gmail.com>
2018-03-16 07:58:27 +0100

    Initial Commit

---
+++ VampProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VampProject
+//
+//  Created by Gusto Nemec on 16/03/2018.
+//  Copyright © 2018 Gusto Nemec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

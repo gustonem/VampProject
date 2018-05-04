dc6d1824b57a74fba204759fbb82d3233410f98b
Tomas Skypala <xskypala@gmail.com>
2018-05-04 02:11:50 +0200

    smart uni point poster ar augmentation kickoff

--- VampProject/Controller/QRLocator/AR/ARLocatorViewController.swift
+++ VampProject/Controller/QRLocator/AR/ARLocatorViewController.swift
@@ -15,31 +15,23 @@ class ARLocatorViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
     
     var isPlaneSelected = false
-//    var anchors = [ARAnchor]()
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        os_log("ARLocatorViewController: renderer called")
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         let referenceImage = imageAnchor.referenceImage
         DispatchQueue.main.async {
             
             // Create a plane to visualize the initial position of the detected image.
-            let plane = SCNPlane(width: referenceImage.physicalSize.width,
-                                 height: referenceImage.physicalSize.height)
+            let plane = SCNPlane(width: referenceImage.physicalSize.width * 1.5,
+                                 height: referenceImage.physicalSize.height * 1.5)
             let planeNode = SCNNode(geometry: plane)
-            planeNode.opacity = 0.25
+            planeNode.opacity = 1
             
-            /*
-             `SCNPlane` is vertically oriented in its local coordinate space, but
-             `ARImageAnchor` assumes the image is horizontal in its local space, so
-             rotate the plane to match.
-             */
+            plane.materials = [SCNMaterial()]
+            plane.materials[0].diffuse.contents = UIImage(named: "Point FI MUNI Main Hall")
+            
             planeNode.eulerAngles.x = -.pi / 2
             
-            /*
-             Image anchors are not tracked after initial detection, so create an
-             animation that limits the duration for which the plane visualization appears.
-             */
 //            planeNode.runAction(self.imageHighlightAction)
             
             // Add the plane visualization to the scene.
@@ -88,11 +80,6 @@ class ARLocatorViewController: UIViewController, ARSCNViewDelegate {
         if let geometryNode = node.childNodes.first {
             if node.childNodes.count > 0 {
                 geometryNode.removeFromParentNode()
-//                geometryNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "./art.scnassets/wood.png")
-//                geometryNode.geometry?.firstMaterial?.locksAmbientWithDiffuse = true
-//                geometryNode.geometry?.firstMaterial?.diffuse.wrapS = SCNWrapMode.repeat
-//                geometryNode.geometry?.firstMaterial?.diffuse.wrapT = SCNWrapMode.repeat
-//                geometryNode.geometry?.firstMaterial?.diffuse.mipFilter = SCNFilterMode.linear
             }
         }
     }
@@ -134,24 +121,7 @@ class ARLocatorViewController: UIViewController, ARSCNViewDelegate {
             .fadeOpacity(to: 0.85, duration: 0.25),
             .fadeOpacity(to: 0.15, duration: 0.25),
             .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
             .fadeOpacity(to: 0.15, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.15, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.15, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.15, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.15, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
-            .fadeOpacity(to: 0.15, duration: 0.25),
-            .fadeOpacity(to: 0.85, duration: 0.25),
             .fadeOpacity(to: 0.85, duration: 0.25),
             .fadeOpacity(to: 0.15, duration: 0.25),
             .fadeOpacity(to: 0.85, duration: 0.25),
@@ -160,102 +130,3 @@ class ARLocatorViewController: UIViewController, ARSCNViewDelegate {
             ])
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//import ARKit
-//
-//class ARLocatorViewController: UIViewController, ARSKViewDelegate {
-//
-//    var sceneView: ARSKView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        if let view = self.view as? ARSKView {
-//            sceneView           = view
-//            sceneView!.delegate = self
-//            let scene           = LocatorScene(size: view.bounds.size)
-//            scene.scaleMode     = .resizeFill
-//            scene.anchorPoint   = CGPoint(x: 0.5, y: 0.5)
-//            view.presentScene(scene)
-//            view.showsFPS       = true
-//            view.showsNodeCount = true
-//        }
-//    }
-//
-////    func view(_ view: ARSKView,
-////              nodeFor anchor: ARAnchor) -> SKNode? {
-////        let bug = SKSpriteNode(imageNamed: "bug")
-////        bug.name = "bug"
-////        return bug
-////    }
-//
-//    func session(_ session: ARSession,
-//                 didFailWithError error: Error) {
-//        print("Session Failed - probably due to lack of camera access")
-//    }
-//
-//    func sessionWasInterrupted(_ session: ARSession) {
-//        print("Session interrupted")
-//    }
-//
-//    func sessionInterruptionEnded(_ session: ARSession) {
-//        print("Session resumed")
-//
-//        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
-//            fatalError("Missing expected asset catalog resources.")
-//        }
-//
-//        let configuration = ARWorldTrackingConfiguration()
-//        configuration.detectionImages = referenceImages
-//        sceneView.session.run(configuration,
-//                              options: [.resetTracking,
-//                                        .removeExistingAnchors])
-////        sceneView.session.run(session.configuration!,
-////                              options: [.resetTracking,
-////                                        .removeExistingAnchors])
-//    }
-//
-//    override var shouldAutorotate: Bool {
-//        return true
-//    }
-//
-//    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-//        if UIDevice.current.userInterfaceIdiom == .phone {
-//            return .allButUpsideDown
-//        } else {
-//            return .all
-//        }
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        let configuration = ARWorldTrackingConfiguration()
-//        sceneView.session.run(configuration)
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        sceneView.session.pause()
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Release any cached data, images, etc that aren't in use.
-//    }
-//
-//    override var prefersStatusBarHidden: Bool {
-//        return true
-//    }
-//}
